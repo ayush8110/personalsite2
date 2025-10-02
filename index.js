734e66be9b022f2ad19cc0a702bc1fe10f469d24
Ayush haldankar <haldankararpita50@gmail.com>
2025-10-02 23:58:46 +0530

    hi

---
+++ index.js
@@ -0,0 +1,20 @@
+function scrollTopara(){
+    document.querySelector(".aboutme").scrollIntoView({
+        behavior: "smooth",
+        block:"start"
+    })
+} 
+
+function scrollToprojectAlign(){
+    document.querySelector(".projectsSection").scrollIntoView({
+        behavior:"smooth"
+
+    })
+}
+
+function scrollTolastContainer(){
+    document.querySelector(".lastContainer").scrollIntoView({
+        behavior: "smooth"
+        
+    })
+}
